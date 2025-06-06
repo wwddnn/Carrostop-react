63b5e68c9f37bdb7d65d14c3febf96bb6ad0a2ad
Warley Dias <72942099+wwddnn@users.noreply.github.com>
2025-06-06 15:47:21 -0300

    Criando o componente Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,7 +3,7 @@ import "./styles.css";
 export default function Header() {
   return (
     <header>
-      <div>
+      <div className="ct-container">
         <h1>Carros Top</h1>
       </div>
     </header>
