428b0f775f56107887cc2c33e9e66a37d9f666f1
Warley Dias <72942099+wwddnn@users.noreply.github.com>
2025-06-06 16:24:56 -0300

    Estruturando a pagina Catalog

---
+++ src/components/Searchbar/index.tsx
@@ -0,0 +1,11 @@
+import "./styles.css";
+
+export default function Searchbar() {
+  
+    return (
+    <form>
+      <input type="text" placeholder="Digite sua busca" />
+      <button type="submit">buscar</button>
+    </form>
+  );
+}
--- src/routes/Catalog/index.tsx
+++ src/routes/Catalog/index.tsx
@@ -1,9 +1,27 @@
 import Header from "../../components/Header";
+import Searchbar from "../../components/Searchbar";
+import './styles.css';
 
 
 export default function Catalog() {
 
     return (
-        <Header />
+        <>
+            <Header />
+            <main className="ct-catalog-main">
+                <section id="ct-catalog-section">
+
+                    <div className="ct-search-bar-container">
+                        <Searchbar />
+                    </div>
+
+                    <div className="ct-catalog-cards-container">
+                        <div>Teste</div>
+
+                    </div>
+
+                </section>
+            </main>
+        </>
     );
 }
